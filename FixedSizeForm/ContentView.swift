c38b0d2b566e8a242387600e9414ad0f73bbfd80
John Siracusa <siracusa@gmail.com>
2025-06-19 11:56:13 -0400

    Initial Commit

---
+++ FixedSizeForm/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FixedSizeForm
+//
+//  Created by Developer on 6/19/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FixedSizeForm/FixedSizeFormApp.swift
@@ -0,0 +1,17 @@
+//
+//  FixedSizeFormApp.swift
+//  FixedSizeForm
+//
+//  Created by Developer on 6/19/25.
+//
+
+import SwiftUI
+
+@main
+struct FixedSizeFormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
